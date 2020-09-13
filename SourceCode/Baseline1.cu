d753327e668e1fbc09b9063bb4f6302c8daf45c8
truongchauhien <truongchauhien@gmail.com>
2020-09-13 17:32:13 +0700

    Add Baseline1 source code.

---
+++ SourceCode/Baseline1.cu
@@ -0,0 +1,248 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <thrust/device_ptr.h>
+#include <thrust/copy.h>
+#include <thrust/sort.h>
+
+#define DEBUG 0
+
+typedef enum { SORT_BY_HOST, SORT_BY_THRUST, SORT_BY_DEVICE } Implementation;
+
+#define CHECK(call)                                                            \
+{                                                                              \
+    const cudaError_t error = call;                                            \
+    if (error != cudaSuccess)                                                  \
+    {                                                                          \
+        fprintf(stderr, "Error: %s:%d, ", __FILE__, __LINE__);                 \
+        fprintf(stderr, "code: %d, reason: %s\n", error,                       \
+                cudaGetErrorString(error));                                    \
+        exit(1);                                                               \
+    }                                                                          \
+}
+
+struct GpuTimer
+{
+    cudaEvent_t start;
+    cudaEvent_t stop;
+
+    GpuTimer()
+    {
+        cudaEventCreate(&start);
+        cudaEventCreate(&stop);
+    }
+
+    ~GpuTimer()
+    {
+        cudaEventDestroy(start);
+        cudaEventDestroy(stop);
+    }
+
+    void Start()
+    {
+        cudaEventRecord(start, 0);
+        cudaEventSynchronize(start);
+    }
+
+    void Stop()
+    {
+        cudaEventRecord(stop, 0);
+    }
+
+    float Elapsed()
+    {
+        float elapsed;
+        cudaEventSynchronize(stop);
+        cudaEventElapsedTime(&elapsed, start, stop);
+        return elapsed;
+    }
+};
+
+void sortByHost(const uint32_t * in, int n, uint32_t * out) {
+    int nBits = 4; // Assume: nBits in {1, 2, 4, 8, 16}
+    int nBins = 1 << nBits; // 2^nBits
+
+    int * hist = (int *)malloc(nBins * sizeof(int));
+    int * histScan = (int *)malloc(nBins * sizeof(int));
+
+    uint32_t * src = (uint32_t *)malloc(n * sizeof(uint32_t));
+    uint32_t * originalSrc = src; // To free memory later
+    memcpy(src, in, n * sizeof(uint32_t));
+    uint32_t * dst = out;
+
+    // Loop from LSD (Least Significant Digit) to MSD (Most Significant Digit)
+    // (Each digit consists of nBits bit)
+	// In each loop, sort elements according to the current digit from src to dst 
+	// (using STABLE counting sort)
+    for (int bit = 0; bit < sizeof(uint32_t) * 8; bit += nBits) {
+    	// TODO: Compute histogram
+        memset(hist, 0, nBins * sizeof(int));
+        for (int i = 0; i < n; i++) {
+            int bin = (src[i] >> bit) & (nBins - 1);
+            hist[bin]++;
+        }
+
+    	// TODO: Scan histogram (exclusively)
+        histScan[0] = 0;
+        for (int bin = 1; bin < nBins; bin++) {
+            histScan[bin] = histScan[bin - 1] + hist[bin - 1];
+        }
+
+    	// TODO: Scatter elements to correct locations
+        for (int i = 0; i < n; i++) {
+            int bin = (src[i] >> bit) & (nBins - 1);
+            dst[histScan[bin]] = src[i];
+            histScan[bin]++;
+        }
+    	
+    	// Swap src and dst
+        uint32_t * temp = src;
+        src = dst;
+        dst = temp;
+    }
+
+    // Copy result to out
+    memcpy(out, src, n * sizeof(uint32_t)); 
+
+    // Free memory
+    free(originalSrc);
+    free(hist);
+    free(histScan);
+}
+
+void sortByThrust(const uint32_t * in, int n, uint32_t * out) {
+    thrust::device_vector<uint32_t> dv_out(in, in + n);
+	thrust::sort(dv_out.begin(), dv_out.end());
+	thrust::copy(dv_out.begin(), dv_out.end(), out);
+}
+
+void sortByDevice(const uint32_t * in, int n, uint32_t * out, int blockSize)
+{
+    // TODO
+}
+
+void sort(const uint32_t * in, int n,
+          uint32_t * out,
+          Implementation implementation = SORT_BY_HOST, int blockSize = 1) {
+    GpuTimer timer; 
+    timer.Start();
+
+    if (implementation == SORT_BY_HOST) {
+    	printf("\nRadix Sort by host\n");
+        sortByHost(in, n, out);
+    } else if (implementation == SORT_BY_THRUST) {
+    	printf("\nRadix Sort by Thrust library\n");
+        sortByThrust(in, n, out);
+    } else {
+        printf("\nRadix Sort by device\n");
+        sortByDevice(in, n, out, blockSize);
+    }
+
+    timer.Stop();
+    printf("Time: %.3f ms\n", timer.Elapsed());
+}
+
+void printDeviceInfo()
+{
+    cudaDeviceProp devProv;
+    CHECK(cudaGetDeviceProperties(&devProv, 0));
+    printf("**********GPU info**********\n");
+    printf("Name: %s\n", devProv.name);
+    printf("Compute capability: %d.%d\n", devProv.major, devProv.minor);
+    printf("Num SMs: %d\n", devProv.multiProcessorCount);
+    printf("Max num threads per SM: %d\n", devProv.maxThreadsPerMultiProcessor); 
+    printf("Max num warps per SM: %d\n", devProv.maxThreadsPerMultiProcessor / devProv.warpSize);
+    printf("GMEM: %zu byte\n", devProv.totalGlobalMem);
+    printf("SMEM per SM: %zu byte\n", devProv.sharedMemPerMultiprocessor);
+    printf("SMEM per block: %zu byte\n", devProv.sharedMemPerBlock);
+    printf("****************************\n");
+}
+
+void checkCorrectness(uint32_t * out, uint32_t * correctOut, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (out[i] != correctOut[i])
+        {
+            printf("INCORRECT :(\n");
+            return;
+        }
+    }
+    printf("CORRECT :)\n");
+}
+
+void printArray(uint32_t * a, int n)
+{
+    for (int i = 0; i < n; i++)
+        printf("%i ", a[i]);
+    printf("\n");
+}
+
+int main(int argc, char ** argv)
+{
+    // PRINT OUT DEVICE INFO
+    printDeviceInfo();
+
+    // SET UP INPUT SIZE
+    int n;
+    if (DEBUG) {
+        n = 513;
+    } else {
+        n = (1 << 24) + 1;
+    }
+    printf("\nInput size: %d\n", n);
+
+    // ALLOCATE MEMORIES
+    size_t bytes = n * sizeof(uint32_t);
+    uint32_t * in = (uint32_t *)malloc(bytes);
+    uint32_t * out = (uint32_t *)malloc(bytes); // Device result
+    uint32_t * correctOut = (uint32_t *)malloc(bytes); // Host result
+
+    // SET UP INPUT DATA
+    for (int i = 0; i < n; i++)
+    {
+        if (DEBUG) {
+            in[i] = rand() % 255;
+        } else {
+            in[i] = rand();
+        }
+    }
+    if (DEBUG) {
+        printArray(in, n);
+    }
+
+    // DETERMINE BLOCK SIZE
+    int blockSize = 512;
+    if (argc == 2) {
+        blockSize = atoi(argv[1]);
+    }
+
+    // Sorting by Host
+    sort(in, n, correctOut);
+    if (DEBUG) {
+        printArray(correctOut, n);
+    }
+
+    // Sorting by Thrust Library
+    sort(in, n, out, SORT_BY_THRUST);
+    if (DEBUG) {
+        printArray(out, n);
+    }
+    checkCorrectness(out, correctOut, n);
+    memset(out, 0u, n * sizeof(uint32_t)); // Reset ouput.
+
+    // Sorting by Device
+    sort(in, n, out, SORT_BY_DEVICE, blockSize);
+    if (DEBUG) {
+        printArray(out, n);
+    }
+    checkCorrectness(out, correctOut, n);
+
+    // FREE MEMORIES
+    free(in);
+    free(out);
+    free(correctOut);
+    
+    return EXIT_SUCCESS;
+}
